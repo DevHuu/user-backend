587f7b3f11e3e9f359d04016919387e39a480743
liujianhui <liujianhui@landray.com.cn>
2024-11-07 16:21:28 +0800

    API参数优化

---
+++ src/main/java/com/hui/userbackend/mapper/UnitMapper.java
@@ -0,0 +1,18 @@
+package com.hui.userbackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hui.userbackend.model.domain.Unit;
+
+/**
+* @author jklanling
+* @description 针对表【unit】的数据库操作Mapper
+* @createDate 2024-11-06 23:11:33
+* @Entity generator.domain.Unit
+*/
+public interface UnitMapper extends BaseMapper<Unit> {
+
+}
+
+
+
+
